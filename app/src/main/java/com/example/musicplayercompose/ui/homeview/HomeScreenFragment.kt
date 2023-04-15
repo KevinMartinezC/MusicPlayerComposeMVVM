3b7f008d1e68bab5474a59019a29673c0768f2bd
KevinMartinezC <90873838+KevinMartinezC@users.noreply.github.com>
2023-04-14 20:10:10 -0600

    feat: add buttomBar in the HomeScreenFragment

--- app/src/main/java/com/example/musicplayercompose/ui/homeview/HomeScreenFragment.kt
+++ app/src/main/java/com/example/musicplayercompose/ui/homeview/HomeScreenFragment.kt
@@ -7,17 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Shuffle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -34,6 +45,7 @@ import coil.compose.rememberImagePainter
 import com.example.musicplayercompose.R
 import com.example.musicplayercompose.model.Song
 import com.example.musicplayercompose.model.SongRepository
+import com.example.musicplayercompose.model.SongRepository.songs
 import com.example.musicplayercompose.model.media.MediaPlayerHolder
 import com.example.musicplayercompose.ui.homeview.viewmodel.HomeScreenViewModel
 import com.example.musicplayercompose.ui.homeview.viewmodel.HomeScreenViewModelFactory
@@ -56,14 +68,21 @@ class HomeScreenFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                MaterialTheme {
-                    val songs by viewModel.uiState.songsStateFlow.collectAsState()
-                    SongList(songs) { song ->
+                val songs by viewModel.uiState.songsStateFlow.collectAsState()
+                Scaffold(
+                    bottomBar = { BottomBarActions(onSettingsClick = { navigateToSettings() }) }
+                ) { paddingValues ->
+                    SongList(songs, paddingValues) { song ->
                         onSongClick(song)
                     }
                 }
             }
+
         }
+    }
+
+    private fun navigateToSettings() {
+        findNavController().navigate(R.id.action_homeScreenFragment_to_settingScreenFragment)
     }
 
     private fun onSongClick(song: Song) {
@@ -93,14 +112,59 @@ class HomeScreenFragment : Fragment() {
 }
 
 @Composable
-fun SongList(songs: List<Song>, onSongClick: (Song) -> Unit) {
-    LazyColumn {
+fun SongList(songs: List<Song>, paddingValues: PaddingValues, onSongClick: (Song) -> Unit) {
+    LazyColumn(
+        modifier = Modifier.padding(paddingValues),
+        contentPadding = PaddingValues(
+            horizontal = 0.dp,
+            vertical = 8.dp
+        ) // Adjust the padding values as needed
+    ) {
         items(songs) { song ->
             SongListItem(song, onSongClick)
             Divider()
         }
     }
 }
+
+
+@Composable
+fun BottomBarActions(onSettingsClick: () -> Unit) {
+    BottomAppBar {
+        IconButton(
+            onClick = onSettingsClick
+
+        ) {
+            Icon(
+                Icons.Filled.Settings,
+                contentDescription = stringResource(R.string.settings)
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        IconButton(
+            onClick = {
+                // Handle random song button click
+            }
+        ) {
+            Icon(
+                Icons.Filled.Shuffle,
+                contentDescription = stringResource(R.string.random_song)
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        IconButton(
+            onClick = {
+                // Handle play first song button click
+            }
+        ) {
+            Icon(
+                Icons.Filled.PlayArrow,
+                contentDescription = stringResource(R.string.play_first_song)
+            )
+        }
+    }
+}
+
 @Composable
 fun SongListItem(song: Song, onClick: (Song) -> Unit) {
     Row(
